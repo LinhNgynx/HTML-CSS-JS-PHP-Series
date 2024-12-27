44ac6314f4b0cf7a71bd44411ac9bb23f41bc501
LinhNgynx <iamlinhuwu@gmail.com>
2024-12-27 15:42:52 +0700

    second commit of the day

--- Products Filter and Search/connection.php
+++ Products Filter and Search/connection.php
@@ -1,7 +1,7 @@
 <?php 
 $hostname='localhost';
 $username='root';
-$password='65770310';
+$password='';
 $dbname='shop';
 $conn=new mysqli($hostname, $username, $password, $dbname);
 if($conn->connect_error){
